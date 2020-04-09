3a8c3c78b2eddc2a55dd13acf915a01c6a2713a0
Woohyeok Choi <woohyeok.choi@kaist.ac.kr>
2020-04-09 14:14:20 +0900

    Init

--- beactive/__init__.py
+++ beactive/__init__.py
@@ -1,7 +1,6 @@
-from .preprocess import AppUsageProcessor, ActivityProcessor, DataProcessor, BatteryProcessor, CallLogProcessor, \
-    ConnectivityProcessor, DataTrafficProcessor, LocationProcessor, MessageProcessor, RingerModeProcessor, \
-    ScreenProcessor, TimeProcessor, NotificationProcessor, process_data, ProcessedData
-
-from .dbstream import DBStream
-
-from .validation import Model, Wrapper, Score
+__all__ = [
+    'model',
+    'preprocess',
+    'util',
+    'validation'
+]
